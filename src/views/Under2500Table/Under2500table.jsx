8a02412b02f515dc8296ddd64c62b5aaec8c5273
Pepe <skpepe134@gmail.com>
2018-02-11 07:41:14 +0700

    add new global var link

--- src/views/Under2500Table/Under2500table.jsx
+++ src/views/Under2500Table/Under2500table.jsx
@@ -1,3 +1,4 @@
+import {global} from 'variables/general';
 import React from 'react';
 import {
     Grid
@@ -6,19 +7,37 @@ import {
 import {
     RegularCard, Table, ItemGrid
 } from 'components';
+import 'whatwg-fetch';
 
 class Under2500Table extends React.Component{
     constructor(){
         super()
-        this.state = {
+        this.state = { 
             sbjArr : [
                 {sbj:'Math',num:1,color:'blue'} ,
                 {sbj:'Physics',num:2,color:'blue'} ,
                 {sbj:'Chemistry',num:3,color:'blue'} ,
-                // {sbj:'English',num:4} ,
+                {sbj:'English',num:4 , color : 'blue'} ,
                 // {sbj:'Bio',num:5}
             ]
         }
+    }
+    componentDidMount(){
+        fetch(global.postlink+'/post/v1/getUnder2500', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json'
+            //   'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              name: 'Hubot',
+              login: 'hubot',
+            })
+        }).then(data=>{return data.json()}).then(data=>{
+            var tmp = this.state;
+            tmp.data = data;
+            this.setState(tmp);
+        })
     }
     render(){
         return (
@@ -26,7 +45,7 @@ class Under2500Table extends React.Component{
                 {
                     this.state.sbjArr.map((obj)=>{
                         return (
-                            <ItemGrid xs={12} sm={6} md={4} key={obj.sbj}>
+                            <ItemGrid xs={12} sm={6} md={6} key={obj.sbj}>
                                 <RegularCard
                                     cardTitle={obj.sbj}
                                     headerColor={obj.color}
@@ -34,15 +53,10 @@ class Under2500Table extends React.Component{
                                     content={
                                         <Table
                                             tableHeaderColor="primary"
-                                            tableHead={['Name','Country','City','Salary']}
-                                            tableData={[
-                                                [ "Dakota Rice" , "Niger" , "Oud-Turnhout" , "$36,738" ] ,
-                                                [ "Minerva Hooper" , "Curaçao" , "Sinaai-Waas" , "$23,789" ] ,
-                                                [ "Sage Rodriguez" , "Netherlands" , "Baileux" , "$56,142" ] ,
-                                                [ "Philip Chaney" , "Korea, South" , "Overland Park" , "$38,735" ] ,
-                                                [ "Doris Greene" , "Malawi" , "Feldkirchen in Kärnten" , "$63,542" ] ,
-                                                [ "Mason Porter" , "Chile" , "Gloucester" , "$78,615" ]
-                                            ]}
+                                            tableHead={['ID','Name','Balance']}
+                                            tableData={this.state.data?this.state.data.arr.filter(e=>{return e.subject === obj.sbj[0]}).map(e=>{
+                                                return [''+e.studentID,e.firstname+'('+e.nickname+')',''+e.total];
+                                            }) : []}
                                         />
                                     }
                                 />
@@ -50,26 +64,6 @@ class Under2500Table extends React.Component{
                         )
                     })
                 }
-                <ItemGrid xs={12} sm={6} md={4}>
-                    <RegularCard
-                        cardTitle="Under2500"
-                        cardSubtitle="Here is a subtitle for this table"
-                        content={
-                            <Table
-                                tableHeaderColor="primary"
-                                tableHead={['Name','Country','City','Salary']}
-                                tableData={[
-                                    [ "Dakota Rice" , "Niger" , "Oud-Turnhout" , "$36,738" ] ,
-                                    [ "Minerva Hooper" , "Curaçao" , "Sinaai-Waas" , "$23,789" ] ,
-                                    [ "Sage Rodriguez" , "Netherlands" , "Baileux" , "$56,142" ] ,
-                                    [ "Philip Chaney" , "Korea, South" , "Overland Park" , "$38,735" ] ,
-                                    [ "Doris Greene" , "Malawi" , "Feldkirchen in Kärnten" , "$63,542" ] ,
-                                    [ "Mason Porter" , "Chile" , "Gloucester" , "$78,615" ]
-                                ]}
-                            />
-                        }
-                    />
-                </ItemGrid>
             </Grid>
         );
     }
